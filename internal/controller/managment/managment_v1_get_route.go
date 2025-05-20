1a00511e15fc00db0812b214cc0edacfa1b3c6b5
Allen <yinhaozheng77625961@gmail.com>
2025-05-20 13:30:54 +0700

    update

--- internal/controller/managment/managment_v1_get_route.go
+++ internal/controller/managment/managment_v1_get_route.go
@@ -24,41 +24,118 @@ func (c *ControllerV1) GetRoute(ctx context.Context, req *v1.GetRouteReq) (res *
 var hardCodeResponse string = `
 {
     "success": true,
-    "data": {
-        "path": "/robot",
-        "meta": {
-            "icon": "ep:user-filled",
-            "title": "机器人管理",
-            "rank": 2
-        },
-        "children": [
-            {
-                "path": "/robot/base/index",
-                "name": "baseSettings",
-                "meta": {
-                    "icon": "ep:baseball",
-                    "title": "基础管理",
-                    "roles": [
-                        "admin",
-                        "common"
-                    ],
-                    "showParent": true
-                }
+    "data": [
+        {
+            "path": "/system",
+            "meta": {
+                "icon": "ri:settings-3-line",
+                "title": "menus.pureSysManagement",
+                "rank": 14
             },
-            {
-                "path": "/robot/operations/index",
-                "name": "operationRecords",
-                "meta": {
-                    "icon": "ep:baseball",
-                    "title": "操作记录",
-                    "roles": [
-                        "admin",
-                        "common"
-                    ],
-                    "showParent": true
+            "children": [
+                {
+                    "path": "/system/user/index",
+                    "name": "SystemUser",
+                    "meta": {
+                        "icon": "ri:admin-line",
+                        "title": "menus.pureUser",
+                        "roles": [
+                            "admin"
+                        ]
+                    }
                 }
-            }
-        ]
-    }
+            ]
+        },
+        {
+            "path": "/roles",
+            "meta": {
+                "icon": "ep:user-filled",
+                "title": "角色权限设置",
+                "rank": 2
+            },
+            "children": [
+                {
+                    "path": "/roles/permission/index",
+                    "name": "rolePermissionSettings",
+                    "meta": {
+                        "icon": "ep:baseball",
+                        "title": "角色权限设置",
+                        "roles": [
+                            "admin"
+                        ],
+                        "showParent": true
+                    }
+                }
+            ]
+        },
+        {
+            "path": "/groups",
+            "meta": {
+                "icon": "ep:user-filled",
+                "title": "群组管理",
+                "rank": 2
+            },
+            "children": [
+                {
+                    "path": "/groups/management/index",
+                    "name": "groupManagement",
+                    "meta": {
+                        "icon": "ep:baseball",
+                        "title": "群组管理",
+                        "roles": [
+                            "admin",
+                            "common"
+                        ],
+                        "showParent": true
+                    }
+                }
+            ]
+        },
+		{
+			"path": "/robot",
+			"meta": {
+				"icon": "ep:user-filled",
+				"title": "机器人管理",
+				"rank": 2
+			},
+			"children": [
+				{
+					"path": "/robot/base/index",
+					"name": "baseSettings",
+					"meta": {
+						"icon": "ep:baseball",
+						"title": "基础管理",
+						"roles": [
+							"admin",
+							"common"
+						],
+						"showParent": true
+					}
+				},
+			]
+		},
+        {
+            "path": "/controls",
+            "meta": {
+                "icon": "ri:bookmark-2-line",
+                "title": "中控平台",
+                "rank": 1
+            },
+            "children": [
+                {
+                    "path": "/controls/management/index",
+                    "name": "management",
+                    "meta": {
+                        "title": "中控平台管理",
+                        "roles": [
+                            "admin"
+                        ],
+                        "showParent": true
+                    }
+                }
+            ]
+        }
+    ]
 }
+
 `
