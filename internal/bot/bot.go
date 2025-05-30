07465040c0737f4c628645d458350ed47fbf9400
Allen <yinhaozheng77625961@gmail.com>
2025-05-30 22:14:28 +0700

    update

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -19,7 +19,7 @@ import (
 	"time"
 )
 
-var AwesomeBotApiChan = make(chan *tgbotapi.BotAPI, 1)
+var AwesomeBotApiChan = make(chan *tgbotapi.BotAPI, 100)
 
 type GroupPayload struct {
 	ID           string `json:"id"`
